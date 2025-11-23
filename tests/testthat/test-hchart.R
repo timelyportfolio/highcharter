235bb89137be5dae549a4fd82761d267aedb76fb
timelyportfolio <kent.russell@timelyportfolio.com>
2025-11-23 08:48:32 -0600

    remove `igraph` dependency and references

--- tests/testthat/test-hchart.R
+++ tests/testthat/test-hchart.R
@@ -41,26 +41,6 @@ test_that("hchart returns a valid time series decomposition plot after valid dat
   expect_true(all(class(h) %in% c("highchart", "htmlwidget")))
 })
 
-test_that("hchart returns a valid graph after valid data input", {
-  require(igraph)
-  N <- 40
-
-  net <- sample_gnp(N, p = 2 / N)
-  wc <- cluster_walktrap(net)
-
-  V(net)$label <- seq(N)
-  V(net)$name <- paste("I'm #", seq(N))
-  V(net)$page_rank <- round(page.rank(net)$vector, 2)
-  V(net)$betweenness <- round(betweenness(net), 2)
-  V(net)$degree <- degree(net)
-  V(net)$size <- V(net)$degree
-  V(net)$comm <- membership(wc)
-  V(net)$color <- colorize(membership(wc))
-
-  h <- hchart(net, layout = layout_with_fr)
-  expect_true(all(class(h) %in% c("highchart", "htmlwidget")))
-})
-
 test_that("hchart returns a valid model plot after valid data input", {
   library(survival)
 
